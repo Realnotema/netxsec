db58b304201a8bfb4878d71383a6070107c1cd52
Realnotema <tematsyganckov@yandex.ru>
2024-06-17 14:06:09 +0500

    install unix 0.1

--- install_unix.sh
+++ install_unix.sh
@@ -2,15 +2,15 @@
 
 if [[ "$(uname)" == "Linux" ]]; then
     if command -v apt-get &> /dev/null; then
-        sudo apt-get install -y libpcap libnet
+        sudo apt-get install -y make gcc libpcap-dev
     elif command -v yum &> /dev/null; then
-        sudo yum install -y libpcap libnet
+        sudo yum install -y make gcc libpcap-devel
     else
         echo "Can't determine OS. QUITTING!"
         exit 1
     fi
 elif [[ "$(uname)" == "Darwin" ]]; then
-    brew install libpcap libnet 
+    brew install make gcc libpcap 
 else
     echo "Unsupported OS. QUITTING!"
     exit 1
